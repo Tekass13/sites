5884e0b23950420a99f55db849b4f12c55262173
TeKaSs <98315145+Tekass13@users.noreply.github.com>
2024-11-21 11:44:59 +0100

    Add files via upload

---
+++ JS/bre03-js-j3/exercices-objets/ex-1/index.js
@@ -0,0 +1,16 @@
+let student = {
+    name : "Steven",
+    age : 19,
+    grades : [12, 14, 10],
+    average : 0
+};
+
+let sum = 0;
+
+for(let i = 0; i < student.grades.length; i++) {
+    sum = sum + student.grades[i];
+}
+
+student.average = sum / student.grades.length;
+
+console.log(student);
---
+++ JS/bre03-js-j3/exercices-objets/ex-2/index.js
@@ -0,0 +1,7 @@
+let student = {
+    name : "Mari",
+    age : 37,
+    average: 13
+};
+
+console.log(`The student is called ${student.name}, is ${student.age} years old and has an average of ${student.average}`);
