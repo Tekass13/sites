9694508e16cb8a05446b48ee82b8b318fc26051b
tekass13 <tekass56800@gmail.com>
2025-01-09 09:48:55 +0100

    Maj(sites)Maj PHP files

--- PHP/Jour-3/ex-1/create-address.php
+++ PHP/Jour-3/ex-1/create-address.php
@@ -1,22 +1,22 @@
 <?php
-// require('connexion.php');
+require('connexion.php');
 
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//     $street = $_POST['street'] ?? null;
-//     $city = $_POST['city'] ?? null;
-//     $zipcode = $_POST['zipcode'] ?? null;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $street = $_POST['street'] ?? null;
+    $city = $_POST['city'] ?? null;
+    $zipcode = $_POST['zipcode'] ?? null;
 
-//     if ($street && $city && $zipcode) {
-//         $query = $db->prepare('INSERT INTO address (street, city, zipcode) VALUES (:street, :city, :zipcode)');
+    if ($street && $city && $zipcode) {
+        $query = $db->prepare('INSERT INTO address (street, city, zipcode) VALUES (:street, :city, :zipcode)');
 
-//         $query->execute([
-//             'street' => $street,
-//             'city' => $city,
-//             'zipcode' => $zipcode
-//         ]);
+        $query->execute([
+            'street' => $street,
+            'city' => $city,
+            'zipcode' => $zipcode
+        ]);
 
-//         echo "L'adresse a été ajoutée avec succès !";
-//     } else {
-//         echo "Veuillez remplir tous les champs du formulaire.";
-//     }
-// }
+        echo "L'adresse a été ajoutée avec succès !";
+    } else {
+        echo "Veuillez remplir tous les champs du formulaire.";
+    }
+}
